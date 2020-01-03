0e556aa79bcfd76b5633327128b0dda8d756ad3d
sakuya <475693104@qq.com>
2020-01-03 17:23:12 +0800

    第一次提交

---
+++ text/src/text/test.java
@@ -0,0 +1,12 @@
+package text;
+
+public class test {
+		public static void main(String[] args) {
+		    Integer f1 = 100, f2 = 100, f3 = 150, f4 = 150;
+		    System.out.println(f3==f4);
+		    System.out.println(f1==f2);
+		    System.out.println(f3.equals(f4));
+		    System.out.println(f1==100);
+		}
+		
+}
